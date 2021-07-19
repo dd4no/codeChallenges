6237ea6eed8448182659f7d8cdcb5aa57698e33f
Dave Defourneaux <74273226+dd4no@users.noreply.github.com>
2021-07-19 16:14:24 -0400

    Create July15Challenge.cs

---
+++ July15Challenge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Part 1
+        List<int> numbers = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        List<int> result = new List<int>();
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        Console.WriteLine();
+
+        // Part 2
+        int[] numsToSquare = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        for (int i=0; i<numsToSquare.Length; i++)
+        {
+            Console.WriteLine(numsToSquare[i] * numsToSquare[i]);
+        }
+
+
+
+
+        Console.Read();
+    }
+}
