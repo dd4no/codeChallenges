85b663ebd0f60d10fb04ab33b73e2ef7f3fa1c16
Dave Defourneaux <74273226+dd4no@users.noreply.github.com>
2021-08-12 13:10:42 -0400

    Create reverseCase.cs

---
+++ reverseCase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+  
+
+    static void Main(string[] args)
+    {
+        var sentence1 = reverseCase("Happy Birthday");
+        Console.WriteLine(sentence1);
+       // Console.ReadLine();
+       
+        var sentence2 = reverseCase("MANY THANKS");
+        Console.WriteLine(sentence2);
+        //Console.ReadLine();
+
+        var sentence3 = reverseCase("SpOnTaNeOuS");
+        Console.WriteLine(sentence3);
+
+        Console.ReadLine();
+
+        string reverseCase(string str)
+        {
+
+            string result = "";
+
+            foreach (char letter in str)
+            {
+                if (char.IsUpper(letter))
+                {
+                    result += char.ToLower(letter);
+                }
+                else
+                {
+                    result += char.ToUpper(letter);
+                }
+
+            }
+
+            return result;
+        }
+
+
+
+    }
+
+
+}
